601ddd53792b8cd07c24d19ced2c5b9665dd309a
Alex_Khomchenko <khomchenkoalex79@gmail.com>
2019-10-26 16:21:18 +0300

    use init for properties initialization

--- src/main/kotlin/AppProperty.kt
+++ src/main/kotlin/AppProperty.kt
@@ -1,14 +1,20 @@
 import java.util.*
 
 object AppProperty {
-    private val properties = Properties().apply {
-        Thread.currentThread()
-            .contextClassLoader
-            .getResourceAsStream("application.properties")
-            .let {
-                this.load(it)
-            }
+
+    private var properties : Properties
+
+    init {
+        properties = Properties().apply {
+            Thread.currentThread()
+                .contextClassLoader
+                .getResourceAsStream("application.properties")
+                .let {
+                    this.load(it)
+                }
+        }
     }
+
 
     fun getPropertyByName(propertyName: String): String? =
         properties.getProperty(propertyName)
