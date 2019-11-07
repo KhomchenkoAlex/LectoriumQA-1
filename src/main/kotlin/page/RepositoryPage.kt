c1d1890546aff3f82c5fd41500f68c2103299aa9
Alex_Khomchenko <khomchenkoalex79@gmail.com>
2019-11-08 01:16:30 +0200

    add check number of files and directories tests

--- src/main/kotlin/page/RepositoryPage.kt
+++ src/main/kotlin/page/RepositoryPage.kt
@@ -1,21 +1,24 @@
 package page
 
-import org.openqa.selenium.By
-import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
-import org.openqa.selenium.support.FindAll
 import org.openqa.selenium.support.FindBy
-import org.openqa.selenium.support.PageFactory
-import org.openqa.selenium.support.ui.ExpectedConditions
-import org.openqa.selenium.support.ui.WebDriverWait
-import java.time.Duration
-import java.util.concurrent.TimeUnit
 
 class RepositoryPage(): PageObject() {
 
     @FindBy(xpath = "//tr/td/img")
     private lateinit var listElements: MutableList<WebElement>
 
-    fun getRepositoryElementsNumber() = listElements.size
+    @FindBy(xpath = "//*[local-name() = 'svg'][@aria-label='file']")
+    private lateinit var fileElements: MutableList<WebElement>
+
+
+    @FindBy(xpath = "//*[local-name() = 'svg'][@aria-label='directory']")
+    private lateinit var directoryElements: MutableList<WebElement>
+
+    fun getAllRepositoryElementsNumber() = listElements.size
+
+    fun getFileElementsNumber() = fileElements.size
+
+    fun getDirectoryElementsNumber() = directoryElements.size
 
 }
--- src/test/kotlin/RepositoryTest.kt
+++ src/test/kotlin/RepositoryTest.kt
@@ -1,14 +1,24 @@
 import org.testng.Assert
 import org.testng.annotations.Test
-import page.RepositoryPage
 import java.util.concurrent.TimeUnit
 
 class RepositoryTest: BaseTest() {
 
     @Test
-    fun repositoryTest() {
+    fun checkAllElementsNumber() {
         githubPage.clickOnRepositoryListElement()
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS)
-        Assert.assertEquals(repoPage.getRepositoryElementsNumber(), 7)
+        Assert.assertEquals(repoPage.getAllRepositoryElementsNumber(), 7)
     }
+
+    @Test
+    fun checkFilesNumber(){
+        Assert.assertEquals(repoPage.getFileElementsNumber(), 5)
+    }
+
+    @Test
+    fun checkDirectoriesNumber(){
+        Assert.assertTrue(repoPage.getDirectoryElementsNumber()==2)
+    }
+    
 }
